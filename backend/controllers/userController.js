424e11cda73a4c681cb764947ffe659977f7de95
mirza-sync <mirzasyahmi123@gmail.com>
2024-05-27 23:57:37 +0800

    Add custom error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,9 @@
+import expressAsyncHandler from "express-async-handler"
 // @desc Auth user/set token
 // route POST api/users/auth
 // @access Public
-const authUser = async (req, res) => {
+const authUser = expressAsyncHandler((req, res) => {
     res.status(200).json({ message: 'Auth user' })
-}
+})
 
 export { authUser }
