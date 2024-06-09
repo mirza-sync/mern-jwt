e40ae5c0aca4e6e4d8dffecdb0dd2080357eeaac
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-10 06:47:29 +0800

    Feat: Update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,8 +80,29 @@ const getUserProfile = expressAsyncHandler((req, res) => {
 // @desc Update user profile
 // route PUT api/users/profle
 // @access Private
-const updateUserProfile = expressAsyncHandler((req, res) => {
-    res.status(200).json({ message: 'Update user profile' })
+const updateUserProfile = expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+        console.log(updatedUser)
+
+        return res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
 })
 
 export {
