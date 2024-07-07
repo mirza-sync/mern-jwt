008577bfca3c5fd847afdb1f8bba98082f74e45b
mirza-sync <mirzasyahmi123@gmail.com>
2024-07-07 23:55:40 +0800

    Add login and register page

---
+++ frontend/src/components/FormContainer.tsx
@@ -0,0 +1,19 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+type FormContainerProps = {
+  children: React.ReactNode
+}
+
+const FormContainer = ({ children }: FormContainerProps) => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col xs={12} md={6} className="card p-5">
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default FormContainer
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen.tsx'
+import LoginScreen from './screens/LoginScreen.tsx'
+import RegisterScreen from './screens/RegisterScreen.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route index={true} path="/login" element={<LoginScreen />} />
+      <Route index={true} path="/register" element={<RegisterScreen />} />
     </Route>
   )
 )
