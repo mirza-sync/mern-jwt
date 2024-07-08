37d6e1e143b1f8f7eea282a89e95d5374d27066a
mirza-sync <mirzasyahmi123@gmail.com>
2024-07-08 23:21:56 +0800

    Setup redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,8 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import HomeScreen from './screens/HomeScreen.tsx'
 import LoginScreen from './screens/LoginScreen.tsx'
 import RegisterScreen from './screens/RegisterScreen.tsx'
+import { Provider } from 'react-redux'
+import store from './store.ts'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,7 +21,9 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </Provider>
 )
