bd208cec88f08b04b9087ea343f418f3250d20dd
mirza-sync <mirzasyahmi123@gmail.com>
2024-07-15 22:49:26 +0800

    Add an ugly header fr

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,32 +1,36 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa"
-import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux"
+
 
 const Header = () => {
+  const { userInfo } = useSelector((state: any) => state.auth)
+
   return (
-    <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>MERN App</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaSignInAlt />Login
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/register">
-                <Nav.Link>
-                  <FaSignOutAlt />Sign Up
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <header className="w-100 h-[10vh] flex p-8 border-b-2">
+      <div className="w-1/3"></div>
+      <div className="w-1/3 flex justify-center items-center">
+        <h1 className="text-3xl text-center font-bold">Auth Me-rn fr no cap</h1>
+      </div>
+      <div className="w-1/3 flex justify-end items-center">
+        {userInfo ? (
+          <div className="flex gap-2">
+            <p className="text-lg">{userInfo.name}</p>
+            <button
+              type="button"
+              className="bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded"
+            >
+              <FaSignOutAlt />
+            </button>
+          </div>
+        ) : (
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            <FaSignInAlt />
+          </button>
+        )}
+      </div>
     </header>
   )
 }
--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,8 +1,12 @@
+import Header from "../components/Header"
 import Hero from "../components/Hero"
 
 const HomeScreen = () => {
   return (
-    <Hero />
+    <>
+      <Header />
+      <Hero />
+    </>
   )
 }
 
